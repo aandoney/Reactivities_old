1a9036bbe82b3e13a0c669c51fa1b777e258b9ef
Angel Andoney <angel.andoney@unosquare.com>
2020-02-07 11:27:11 -0600

    website skeleton files

--- API/Startup.cs
+++ API/Startup.cs
@@ -28,7 +28,12 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(opt => {
-               opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection")); 
+                opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection")); 
+            });
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
             });
             services.AddControllers();
         }
@@ -46,6 +51,8 @@ namespace API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors("CorsPolicy");
 
             app.UseEndpoints(endpoints =>
             {
