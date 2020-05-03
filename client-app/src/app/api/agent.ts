4f2305ca3446eefc41e3994a2499a0fe47699546
Angel Andoney <angel.andoney@idexpertscorp.com>
2020-05-03 18:51:31 -0500

    add authentication to front end

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -2,14 +2,26 @@ import axios, { AxiosResponse } from "axios";
 import { IActivity } from "../models/activity";
 import { history } from "../..";
 import { toast } from "react-toastify";
+import { IUser, IUserFormValues } from "../models/user";
 
 axios.defaults.baseURL = "http://localhost:5000/api";
 
-axios.interceptors.response.use(undefined, error => {
-  if (error.message === "Network Error" && !error.response) {
-    toast.error('Network error - make sure API is running!');
+axios.interceptors.request.use(
+  (config) => {
+    const token = window.localStorage.getItem("jwt");
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-  const { status, data, config } = error.response;  
+);
+
+axios.interceptors.response.use(undefined, (error) => {
+  if (error.message === "Network Error" && !error.response) {
+    toast.error("Network error - make sure API is running!");
+  }
+  const { status, data, config } = error.response;
   if (status === 404) {
     history.push("/notfound");
   }
@@ -21,39 +33,25 @@ axios.interceptors.response.use(undefined, error => {
     history.push("/notfound");
   }
   if (status === 500) {
-    toast.error('Server error - check the terminal for more info!')
+    toast.error("Server error - check the terminal for more info!");
   }
-  throw error;
+  throw error.response;
 });
 
 const responseBody = (response: AxiosResponse) => response.data;
 
 const sleep = (ms: number) => (response: AxiosResponse) =>
-  new Promise<AxiosResponse>(resolve =>
+  new Promise<AxiosResponse>((resolve) =>
     setTimeout(() => resolve(response), ms)
   );
 
 const requests = {
-  get: (url: string) =>
-    axios
-      .get(url)
-      .then(sleep(1000))
-      .then(responseBody),
+  get: (url: string) => axios.get(url).then(sleep(1000)).then(responseBody),
   post: (url: string, body: {}) =>
-    axios
-      .post(url, body)
-      .then(sleep(1000))
-      .then(responseBody),
+    axios.post(url, body).then(sleep(1000)).then(responseBody),
   put: (url: string, body: {}) =>
-    axios
-      .put(url, body)
-      .then(sleep(1000))
-      .then(responseBody),
-  del: (url: string) =>
-    axios
-      .delete(url)
-      .then(sleep(1000))
-      .then(responseBody)
+    axios.put(url, body).then(sleep(1000)).then(responseBody),
+  del: (url: string) => axios.delete(url).then(sleep(1000)).then(responseBody),
 };
 
 const Activities = {
@@ -62,9 +60,18 @@ const Activities = {
   create: (activity: IActivity) => requests.post("/activities", activity),
   update: (activity: IActivity) =>
     requests.put(`/activities/${activity.id}`, activity),
-  delete: (id: string) => requests.del(`/activities/${id}`)
+  delete: (id: string) => requests.del(`/activities/${id}`),
+};
+
+const User = {
+  current: (): Promise<IUser> => requests.get("/user"),
+  login: (user: IUserFormValues): Promise<IUser> =>
+    requests.post(`/user/login`, user),
+  register: (user: IUserFormValues): Promise<IUser> =>
+    requests.post(`/user/register`, user),
 };
 
 export default {
-  Activities
+  Activities,
+  User,
 };
